0657b714d433c9a22035f160c5e651cc990c0c0b
Ali Talip ATASEVER <alitalipatasever@gmail.com>
2022-07-29 16:45:13 +0300

    add page R/W data firebase

--- Harcamalar/app/src/main/java/com/alitalipatasever/harcamalar/Add.java
+++ Harcamalar/app/src/main/java/com/alitalipatasever/harcamalar/Add.java
@@ -1,11 +1,16 @@
 package com.alitalipatasever.harcamalar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Add extends AppCompatActivity {
 
@@ -18,5 +23,18 @@ public class Add extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
